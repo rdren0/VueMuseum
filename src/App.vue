849ad0a22cb5510ae344c0b1c1b6305519de621f
Rachael Drennan <rdrennan0@gmail.com>
2019-07-27 17:24:15 -0600

    Setup App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Gallery/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+import Header from './components/Header.vue';
+import Gallery from './components/Gallery.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Header,
+    Gallery
   }
 }
 </script>
