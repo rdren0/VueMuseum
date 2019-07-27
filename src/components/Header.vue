b1342154869ae5aac7b9bfe12e41ef947d261b3b
Rachael Drennan <rdrennan0@gmail.com>
2019-07-27 17:24:29 -0600

    Add temporary text to Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Welcome To The Virtual Harvard Art Museums</h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    // msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: gainsboro;
+}
+/* ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
