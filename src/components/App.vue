a5b9f3ad7de0efde6536b692b782ee51a807fa11
Rachael Drennan <rdrennan0@gmail.com>
2019-07-30 11:19:51 -0600

    Move location of App

---
+++ src/components/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Header />
+    <Container/>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Container from './Container.vue'
+
+export default { 
+  name: "app",
+  components: {
+    Header,
+    Container
+  },  
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
